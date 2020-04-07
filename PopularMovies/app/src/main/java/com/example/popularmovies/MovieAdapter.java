06815b5b15d1391715da6fd8ffb1d364869aa9af
KasperDelaxson <43139475+KasperDelaxson@users.noreply.github.com>
2020-04-07 13:43:44 +0200

    Adapter
    Adapter

--- PopularMovies/app/src/main/java/com/example/popularmovies/MovieAdapter.java
+++ PopularMovies/app/src/main/java/com/example/popularmovies/MovieAdapter.java
@@ -1,42 +1,63 @@
 package com.example.popularmovies;
 
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder>  {
     private ArrayList<Movie> movies;
+    private Context con;
     public MovieAdapter(ArrayList<Movie> movies){
         this.movies = movies;
-
     }
 
 
     @NonNull
     @Override
     public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.movie_view, parent, false);
+        return new MovieViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {
+        holder.title.setText(movies.get(position).getOriginalTitle());
+        holder.releaseDate.setText(movies.get(position).getReleaseDate());
+        String rating = Float.toString(movies.get(position).getVoteAvg());
+        holder.avgRating.setText(rating);
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return this.movies.size();
     }
 
     public class MovieViewHolder extends RecyclerView.ViewHolder{
+        public TextView title, releaseDate, avgRating;
 
         public MovieViewHolder(@NonNull View itemview){
             super(itemview);
+            /*
+            title = itemview.findViewById(R.id.title);
+            releaseDate = itemview.findViewById(R.id.releaseDate);
+            avgRating = itemview.findViewById(R.id.avgRating);
+            */
+
         }
+
 
     }
 
